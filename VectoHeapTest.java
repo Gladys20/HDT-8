79ee1079877ff4482c2e7668b8658835b4d0ab59
hid15776 <hid15776@uvg.edu.gt>
2016-10-02 21:12:29 -0600

    Create VectoHeapTest.java

---
+++ VectoHeapTest.java
@@ -0,0 +1,33 @@
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class VectorHeapTest {
+    
+     //Test Left
+     
+    @Test
+    public void testLeft() {
+        System.out.println("Left");
+        Object left= null;
+        VectorHeap instance = new VectorHeap();
+        instance.Left(left);
+    }
+
+    
+     //Test Rigth
+     
+    @Test
+    public void testRigth() {
+    	  System.out.println("Rigth");
+          Object rigth= null;
+          VectorHeap instance = new VectorHeap();
+          instance.Rigth(rigth);
+
+    }
+
+    
+
+    
+}
